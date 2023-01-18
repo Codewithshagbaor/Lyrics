be079f6aa01ef36124069d815b51b313eaf11bf2
Shagbaor Agber(Pyman) <67190277+Codewithshagbaor@users.noreply.github.com>
2023-01-18 22:09:56 +0100

    Update views.py

--- Base/views.py
+++ Base/views.py
@@ -5,20 +5,21 @@ def index(request):
   if request.method == 'POST':
     
     song_title = request.POST.get('song_title')
+    artist = request.POST.get('artist')
     
     api_key = 'Your Key'
-    api_url = f'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track={song_title}&apikey={api_key}'
+    api_url = f'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track={song_title}&apikey={api_key}&q_artist={artist}'
     
     response = requests.get(api_url)
-    print(response.json())
-        # try:
-        #     lyrics = data['message']['body']['lyrics']['lyrics_body']
-        #     context = {
-        #         'lyrics':lyrics
-        #     }
-        #     return render(request, 'index.html', context)
-        # except KeyError as e:
-        #     return render(request, 'error.html', {'error': f'{e} not found'})
+    data = response.json()
+    try:
+      lyrics = data['message']['body']['lyrics']['lyrics_body']
+      context = {
+        'lyrics':lyrics
+        }
+      return render(request, 'index.html', context)
+    except KeyError as e:
+      return render(request, 'error.html', {'error': f'{e} not found'})
   else:
     return render(request, 'index.html')
   return render(request, 'index.html')
